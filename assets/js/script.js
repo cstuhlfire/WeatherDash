9bc1e7807cf1640cb0741e5fd78d5eff8ec412fc
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-28 00:23:30 -0500

    add/ Dynamically create elements to display recent list, clear list, save and retrieve from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 // Weather Dash
 
 // Globals
+let storageArray = "cityArray";
 let cityArray = [];
 
 // Query Selectors
+let recentListEl = document.getElementById("recent-list");
+let clearEl = document.getElementById("clear");
 let btnEl = document.querySelector(".btn");
-let userCityEl = document.querySelector(".user-city-name");
+let inputCityEl = document.querySelector(".user-city-name");
 
 // Function calls
+init();
 
 // Function definitions
+function init() {
+    // Get recent search cities from localStorage
+    let tempArray = JSON.parse(localStorage.getItem(storageArray));
+
+    if (tempArray !== null) {
+        cityArray = tempArray;
+        renderRecentList();
+    }
+}
+
 function addToCityArray(city) {
     // Check if city is already in array
     // If not in array add city to front of array
@@ -24,25 +38,66 @@ function addToCityArray(city) {
         cityArray.splice(found, 1);
         cityArray.unshift(city);
     }
+    cityArray.splice(8);
+}
+
+function renderRecentList() {    
+    // Clear recent list
+    recentListEl.innerHTML = "";
+
+    // Render li for each element in the cityArray
+    for (let i = 0; i < cityArray.length; i++) {
+
+        let liEl = document.createElement("li");
+
+        liEl.textContent = cityArray[i];
+        liEl.classList.add("list-group-item");
+        recentListEl.appendChild(liEl);
+    }
+}
+
+function storeRecentList() {
+    // stringify the cityArray and store it in localStorage
+    localStorage.setItem(storageArray, JSON.stringify(cityArray));
+}
+
+function clearRecentList() {
+    cityArray.splice(0);
+    renderRecentList();
+    storeRecentList();
 }
 
 function readCityName() {
-    addToCityArray(userCityEl.value);
+    
+    addToCityArray(inputCityEl.value.trim());
+    renderRecentList();
+    storeRecentList();
+    
+    // Clear input value on screen
+    inputCityEl.value = "";
+    inputCityEl.focus();
 }
 
 // Event Listeners
 btnEl.addEventListener("click", readCityName);
+clearEl.addEventListener("click", clearRecentList);
 
 
 // On open...
-// Populate recent search array from localStorage
-// Create and append elements for the recent searches from the recent search array
+// Populate recent search array from localStorage***
+// Create and append elements for the recent searches from the recent search array***
 
 // On click of search button...
 // Capture search city***
 // Add searched city to the front of the recent list array if it's not already in the list***
-// Create and append elements for the recent searches from the recent search array
-// Save recent list array to localStorage
+// Create and append elements for the recent searches from the recent search array***
+// Save recent list array to localStorage***
+
+// On click of clear list...
+// Remove clicked city from recent list array***
+// Create and append elements for the recent searches from the recent search array***
+// Save recent list array to localStorage***
+
 // Use search city to build url for weather service
 // Fetch results from weather service for six days (today and 5 day forecast)
 // Parse results and store in an array of weather objects
@@ -60,7 +115,3 @@ btnEl.addEventListener("click", readCityName);
 // Capture city from recent search button
 // Fetch results from weather service ...
 
-// On click of recent city delete...
-// Remove clicked city from recent list array
-// Create and append elements for the recent searches from the recent search array
-// Save recent list array to localStorage
